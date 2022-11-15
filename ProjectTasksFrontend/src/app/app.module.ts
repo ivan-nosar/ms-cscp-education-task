06ab68ba6a723048d6d885fb21af2aecc73d20f2
Ivan <ivan.nosar@akvelon.com>
2022-11-15 19:09:02 +0100

    Frontend: Initiate components templates

--- ProjectTasksFrontend/src/app/app.module.ts
+++ ProjectTasksFrontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsListComponent,
+    ProjectDetailsComponent,
+    TaskDetailsComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
