d296e8f09769bc8c6c69ebfba3778721800e8724
Ivan <ivan.nosar@akvelon.com>
2022-11-16 22:17:39 +0100

    Frontend: Fix environment configs

--- ProjectTasksFrontend/src/environments/environment.prod.ts
+++ ProjectTasksFrontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: true
+    production: true,
+    apiUrl: `https://projecttasksapilinuxappservice.azurewebsites.net/`,
 };
--- ProjectTasksFrontend/src/environments/environment.ts
+++ ProjectTasksFrontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
     apiUrl: `https://projecttasksapilinuxappservice.azurewebsites.net/`,
-    // apiUrl: `https://localhost:7000/`,
 };
